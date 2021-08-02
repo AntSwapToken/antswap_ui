7c341106a161f28c1b80945df9724855c204adaf
AntSwap <antswaptoken@gmail.com>
2021-08-02 13:28:39 -0400

    More

--- src/views/Profit/components/CompoundModal.tsx
+++ src/views/Profit/components/CompoundModal.tsx
@@ -23,7 +23,7 @@ const CompoundModal: React.FC<DepositModalProps> = ({ earnings, onConfirm, onDis
 
   return (
     <Modal
-      title={`${TranslateString(999, 'Compound')} STONK Earned`}
+      title={`${TranslateString(999, 'Compound')} ANT Earned`}
       onDismiss={onDismiss}
     >
       <BalanceRow>
