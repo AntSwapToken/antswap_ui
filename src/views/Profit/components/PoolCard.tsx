10b3342091258b7710cea233c968f7c0fb3f4e23
AntSwap <antswaptoken@gmail.com>
2021-08-04 15:47:28 -0400

    m

--- src/views/Profit/components/PoolCard.tsx
+++ src/views/Profit/components/PoolCard.tsx
@@ -195,7 +195,7 @@ const PoolCard: React.FC<HarvestProps> = ({ pool }) => {
         <Text style={{ fontSize: '12px' }}>{TranslateString(10001, 'Deposit Fee')}:</Text>
         <Text bold style={{ fontSize: '12px' }}>10 %</Text>
         <Text style={{ fontSize: '12px' }}>{TranslateString(10007, 'Multiplier')}:</Text>
-        <Text bold style={{ fontSize: '12px' }}>20x</Text>
+        <Text bold style={{ fontSize: '12px' }}>100x</Text>
       </Flex>
         <StyledCardActions>
           {!account && <UnlockButton />}
